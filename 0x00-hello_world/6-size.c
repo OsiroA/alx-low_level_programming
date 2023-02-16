b8d08bf33defb51690cbd27fc022585995590196
OsiroA <osiroasunde@gmail.com>
2023-02-16 22:04:03 +0100

    This is a C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@ int main(void)
 	double d;
 	char c;
 
-	printf("The size of an int is: %lu. In", (unsigned long)sizeof(i));
-	printf ("The size of an double is: %lu. \n", , (unsigned long)sizeof(d));
-	printf("The size of an c is: %lu. \n", (unsigned long)sizeof(c));
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf ("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
 
 	return (0);
 }
