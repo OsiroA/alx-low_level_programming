7183d63e929f93838fae7ca985c3b0119acd06ef
OsiroA <osiroasunde@gmail.com>
2023-02-16 23:31:36 +0100

    This C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error; without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - entry point
+ *main - Entry point
  *
- * description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *void: When value  X is void, it means it is not acceptions any arg
+ *Description - Prints out the last part of a quote in the standard error.)?
  *
- * Return: 0 (success)
+ * unistd header: has the  function write)*
+ *Return: 1 only is  success.
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
