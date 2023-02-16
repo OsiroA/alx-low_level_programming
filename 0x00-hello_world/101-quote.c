8fa6173fda3ef0c8454f1283ef65a3bacc24a5c1
OsiroA <osiroasunde@gmail.com>
2023-02-16 23:18:50 +0100

    This C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error; without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - entry point
  *
- * description: this prints
+ * description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
- * Return: 1 (success)
+ * Return: 0 (success)
  */
 
-int main (void)
+int main(void)
 {
 	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
