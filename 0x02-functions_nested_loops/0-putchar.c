75dc73496f62cca4fbaaa697eecbf5a388752e32
OsiroA <osiroasunde@gmail.com>
2023-02-21 17:10:45 +0100

    trying to see if I can get an stderr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -18,6 +18,15 @@ int main(void)
 	{
 		_putchar(l[i]);
 	}
+	_putchar('A');
+	_putchar('n');
+	_putchar('y');
+	_putchar('t');
+	_putchar('h');
+	_putchar('i');
+	_putchar('n');
+	_putchar('g');
+	_putchar('\n');
 
 	return (0);
 }
