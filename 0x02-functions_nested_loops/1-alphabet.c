9301af5501fd97b8e31428e1a05edd0ab3e53e75
OsiroA <osiroasunde@gmail.com>
2023-02-21 21:19:21 +0100

    Betty documentation correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,9 @@
 /* This program prints the alphabet in lowercase, followed by a new line */
 
 /**
- * main - Entry point
- *
  * print_alphabet - prints alphabet in lowercase
  *
- * Return: 0 (Always Success)
+ * Return: void
  */
 void print_alphabet(void)
 {
