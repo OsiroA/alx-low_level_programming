3170b2aceda1de7589ddea2b9f54baf55c243d50
OsiroA <osiroasunde@gmail.com>
2023-02-22 01:11:54 +0100

    first attemt to print 24 hours minute by minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main"
+/* This function prints every minute of the day from 00:00 to 23:59 */
+/**
+ * jack_bauer - prints every minute of the day of jack bauer
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i = 00:00
+	
+	while( i < 24:00)
+	{
+		_putchar (i);
+		i++
+	}
+	return 0;
+}
