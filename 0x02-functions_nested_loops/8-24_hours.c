587e414ba21c410c04569608ecf53a1071f0e0e7
OsiroA <osiroasunde@gmail.com>
2023-02-22 02:00:15 +0100

    another edit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,15 +12,14 @@ void jack_bauer(void)
 
 	for (H = 0; H <= 2; H++)
 	{
-		for (h = 0; h <= 3; h++)
+		for (h = 0; h <= 9; h++)
 		{
-			for (M = 0; M <= 5; M++)
+			for (M = 0; M < 6; M++)
 			{
 				for (m = 0; m <= 9; m++)
 				{
 					if (!((H == 2) && (h > 3)))
 					{
-
 						_putchar ('0' + H);
 						_putchar ('0' + h);
 						_putchar (':');
