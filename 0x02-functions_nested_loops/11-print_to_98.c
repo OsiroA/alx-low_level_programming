2b7ffd18db0460ea9fdfe98f49ba765bc9382bc9
OsiroA <osiroasunde@gmail.com>
2023-02-22 04:15:28 +0100

    used for, if and elseifs

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,8 +11,27 @@
 
 void print_to_98(int n)
 {
-	for (n = 0; n < 99; n++)
+	int m, o;
+
+	if (n <= 98)
 	{
-		_putchar (n);
+		for (m = n; m <= 98; m++)
+		{
+			if (m != 98)
+				printf("%d, ", m);
+			else if (m == 98)
+				printf("%d\n", m);
+		}
+
+	}
+	else if (n > 98)
+	{
+		for (o = n; o >= 98; o--)
+		{
+			if (o != 98)
+				printf("%d, ", o);
+			else if (o == 98)
+				printf("%d\n", o);
+		}
 	}
 }
