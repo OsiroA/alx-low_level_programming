a5c624c68f662a633a6a035e15843d5677ade7e7
OsiroA <osiroasunde@gmail.com>
2023-02-22 02:41:26 +0100

    added fpor statements

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,35 @@
 
 void times_table(void)
 {
-	int a, b, z;
+	int a, b, z, u, d;
 
 	for (a = 0; a <= 9; a++)
 	{
-		if (b = 9)
+		for (b = 0; b <= 9; b++)
 		{
 			z = a * b;
-			_putchar (z);
+
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z-u) / 10;
+
+				_putchar (',');
+				_putchar (' ');
+				_putchar (d + '0');
+				_putchar (u + '0');
+			}
+			else
+			{
+				if (y != 8)
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+				}
+				_putchar (z + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
