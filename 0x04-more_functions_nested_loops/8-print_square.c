f297beb5ad88ab071b7d8a2f6fb9947c96b7e61e
OsiroA <osiroasunde@gmail.com>
2023-02-23 18:53:04 -0800

    tried withan if .. for .. for nested statement

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,9 +7,14 @@
 
 void print_square(int size)
 {
-	char c;
+	int a, b;
 
-	for (c = 0; c < size; c++)
-		_putchar('#');
-	_putchar('\n');
+	if (size < 1)
+		_putchar ('\n');
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+			_putchar ('#');
+		_putchar ('\n');
+	}
 }
