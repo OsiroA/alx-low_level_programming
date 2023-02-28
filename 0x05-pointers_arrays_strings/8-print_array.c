47f1050592ca8f8a9b7f1b9e3173d13b76ae7008
OsiroA <osiroasunde@gmail.com>
2023-02-28 02:08:12 +0100

    This function prints all elements in an array separated by a comma and space

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - This prints n elements of an array
+ * @a: a pointer
+ * @n: an integer
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int f;
+
+	for (f = 0; f < n; f++)
+	{
+		printf("%d", a[f]);
+		if (f != (n-1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
