af686f1c00075e393b8b03c9a059eab4019b9724
OsiroA <osiroasunde@gmail.com>
2023-02-28 21:36:44 +0100

    This function generates random valid password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - This program generates random valid passwords
+ * Return: 0
+ */
+int main(void)
+{
+	int pass[100];
+	int o;
+	int add;
+	int j;
+
+	add = 0;
+
+	srand(time(NULL));
+
+	for (o = 0; o < 100; o++)
+	{
+		pass[o] = rand() % 78;
+		add += (pass[o] + '0');
+		putchar(pass[o] + '0');
+		if ((2772 - add) - '0' < 78)
+		{
+			j = 2772 - add - '0';
+			add += j;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
