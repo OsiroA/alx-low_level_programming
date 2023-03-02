54bfe2a8bcb0ee9ab9245461ceee5e48941d844a
OsiroA <osiroasunde@gmail.com>
2023-03-02 20:17:03 +0100

    first trial of comparation of strings function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcmp - This function compares two strings
+ * @s1: string one
+ * @s2: second string
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int letter = 0;
+	int PresenceOfDifference = 0;
+
+	for (letter = 0; (s1[letter] != '\0' || s2[letter] != '\0'); letter++)
+	{
+		if (s1[letter] != s2[letter])
+			{
+				PresenceOfDifference = 1;
+				break;
+			}
+	}
+	if (PresenceOfDifference == 1)
+	{
+		_putchar ('0');
+	}
+	return (0);
+}
