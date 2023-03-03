6dc8be3c68e80602c4136e99f22ffe00c9a3ba35
OsiroA <osiroasunde@gmail.com>
2023-03-03 05:26:17 +0100

    prints a buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - This function prints a buffer
+ * @b: the buffer to be printed
+ * @size: an integer representing the number of bytes that will
+ * be printed from the buffer
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int memorysize, index;
+
+	for (memorysize = 0; memorysize < size; memorysize++)
+	{
+		printf("%08x: ", memorysize);
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + memorysize) >= size)
+				printf(" ");
+			else
+				printf("%02x", *(b + index + memorysize));
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
+		}
+
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + memorysize) >= size)
+				break;
+
+			else if (*(b + index + memorysize) >= 31 &&
+					*(b + index + memorysize) <= 126)
+				printf("%c", *(b + index + memorysize));
+			else
+			printf(".");
+		}
+
+		if (memorysize >= size)
+		continue;
+
+		printf("\n");
+	}
+
+	if (size <= 0)
+		printf("\n")
+}
