8556984896f482614c5fea2dd1d79e557fc1c617
OsiroA <osiroasunde@gmail.com>
2023-03-07 03:14:14 +0100

    nothing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,26 +5,21 @@
  *
  * Return: 0 (Success)
  */
-
-int main()
+int main(void)
 {
-	for (int i = 0; i <= 8; i++)
-       	{
-		for (int j = i + 1; j <= 9; j++)
-		{
-			printf("%d, %d\n", i, j);
-		}
-       	}
-
-return 0;
+	int tens;
+	int ones;
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = 0; ones <= 9; ones++)
+			if (tens != one)
+				putchar(tens + '0');
+		putchar(ones, tens);
+	}
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+return (0);
 }
-
-/**This program uses two nested for loops to iterate through 
- * all possible combinations of two digits.
- * The outer loop iterates through all digits from 0 to 8,
- * and the inner loop iterates through all digits
- * from the current digit plus 1 to 9. This ensures that the two digits are always different.
- * Inside the inner loop, the program uses printf to print
- * the two digits separated by a comma and a space.
- * When you run this program, it will output all possible combinations of two digits,
- */
