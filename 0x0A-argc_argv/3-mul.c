554b79342f083c0ff40a6be0737537ff01fb24d5
OsiroA <osiroasunde@gmail.com>
2023-03-10 20:50:20 +0100

    This function prints two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - This program multiplies two numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	/* atoi converts a string to an integer */
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
