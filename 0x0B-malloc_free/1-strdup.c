5e465b7ed1cb96e11e9fd74a4d33a4b0a5c61aa3
OsiroA <osiroasunde@gmail.com>
2023-03-13 14:14:24 +0100

    betty and while syntax correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 /**
  * _strdup - This function returns a pointer to a newly allocated space in
- *  memory, which contains a copy of the string given as a parameter
+ * memory, which contains a copy of the string given as a parameter
  * @str: The string given as parameter
  * Return: strcopy or NULL
  */
 char *_strdup(char *str)
 {
 	char *strcopy;
-	int i;
-	int len;
+	unsigned int i;
+	unsigned int len;
 
 	if (str == NULL)
 		return (NULL);
-	/* I want to get the length of str to allocate space to its duplicate*/
+	/*I want to get the length of str to allocate space to its duplicate*/
 	for (len = 0; str[len] != '\0'; len++)
 		;
 	strcopy = malloc(sizeof(char) * (len + 1));
