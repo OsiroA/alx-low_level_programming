a0091f667f7c9959de092e4f05fb3b7e3fe65e4e
OsiroA <osiroasunde@gmail.com>
2023-03-16 20:40:32 +0100

    trying advanced task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: argument counter
+ * Return: 0 or NULL
+ */
+int main(void)
+{
+	int mul, argc = 0, num1 = 0, num2 = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (((num1 >= 'a') && (num1 <= 'z')) || ((num2 >= 'a' && num2 <= 'z')))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
+}
