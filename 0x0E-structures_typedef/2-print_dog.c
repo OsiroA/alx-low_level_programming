a4c7e5e8a29a2064716b94a375cd55afd750125d
OsiroA <osiroasunde@gmail.com>
2023-03-20 22:26:12 +0100

    changed the format specifier back to %f to see if checker would reverse its current feedback;

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,5 +18,5 @@ void print_dog(struct dog *d)
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, *d->age, d->owner);
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
