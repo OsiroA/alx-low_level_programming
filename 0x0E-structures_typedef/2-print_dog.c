05c625a3bc657e64b34f11c8159af88f1d578dc9
OsiroA <osiroasunde@gmail.com>
2023-03-20 22:22:00 +0100

    I changed the format specifier of the age from %f to %g to be able to compare the float to NULL, I also added a return to the first if statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,11 +10,14 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 	{
 		printf("(nil)");
+		return;
 	}
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
+	if (d->age == NULL)
+		printf("Age: (nil)\n");
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Name: %s\nAge: %g\nOwner: %s\n", d->name, d->age, d->owner);
 }
