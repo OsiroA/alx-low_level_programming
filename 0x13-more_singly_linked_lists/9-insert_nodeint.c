2df8e7254b88797b1a9df4a3ecaa51cea4789e31
OsiroA <osiroasunde@gmail.com>
2023-04-03 23:03:30 +0100

    I commented out some parts of the code and added a new while loop...debugging for an unknwn case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,27 +19,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newptr = *head;
 	if (newnode == NULL)
 		return (NULL);
-	newnode->n = n;
-	if (idx == 0)
+	/*newnode->n =n;*/
+	/*if (idx == 0)*/
+	/*{*/
+	/*	newnode->next = *head;*/
+	/*	*head = newnode;*/
+	/*	return (*head);*/
+	/*}*/
+	/*	newnode->next = newptr;*/
+	while (newptr != NULL && index < idx - 1)
 	{
-		newnode->next = newptr;
-		*head = newnode;
-		return (*head);
+		newptr = newptr->next;
+		index++;
 	}
-		newnode->next = newptr;
-	if (index < idx)
-	{
-		for (index = 1; index < idx; index++)
-		{
-			newptr = newptr->next;
-		}
-	}
-	else if (index == idx)
-	{
-		newnode->next = newptr->next;
-		newptr->next = newnode;
-	}
-	else
+	/*else if (index == idx)*/
+	/*{*/
+	/*	newnode->next = newptr->next;*/
+	/*	newptr->next = newnode;*/
+	/*}*/
+	if (newptr == NULL)
 	{
 		free(newnode);
 		return (NULL);
