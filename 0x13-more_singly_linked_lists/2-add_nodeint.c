a566c926bd27b4a3525dcb8782ced7922f23bfbd
OsiroA <osiroasunde@gmail.com>
2023-04-03 18:23:40 +0100

    Trying to solve what the problem may be for a particular unknown test case.

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,9 +14,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 	newnode = *head;
 	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
 	newnode->n = n;
-	newnode->next = NULL;
-
 	newnode->next = *head;
 	*head = newnode;
 
