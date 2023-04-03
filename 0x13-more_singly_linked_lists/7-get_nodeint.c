dda40f35615721f9bf49eecae78142ba9c2e6042
OsiroA <osiroasunde@gmail.com>
2023-04-03 19:17:34 +0100

    changed the position of the if statement to inside the first while loop, this checks the condition as it traverses through each value not just the first node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,10 +9,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int a = 0;
 
-	if (head == NULL)
-		return (NULL);
 	while (a < index)
 	{
+		if (head->next == NULL)
+			return (NULL);
 		while (head->next != NULL)
 		{
 			head = head->next;
