959ceba79e9ec46c762456093cc9b104bac0c7a9
OsiroA <osiroasunde@gmail.com>
2023-04-03 23:05:28 +0100

    I corrected an error...I had an unused parameter

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newptr, *newnode;
 
 	newnode = malloc(sizeof(listint_t));
-	/*newnode->n = n; */
+	newnode->n = n;
 	/*newnode->next = NULL; */
 	newptr = *head;
 	if (newnode == NULL)
