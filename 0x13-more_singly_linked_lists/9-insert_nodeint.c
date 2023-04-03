ca27c4c327ca71ef9783f114ce42641bba0009eb
OsiroA <osiroasunde@gmail.com>
2023-04-03 20:49:54 +0100

    This function inserts a new node at a given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+* insert_nodeint_at_index - This function inserts a new node at a
+* given position
+* @head: a pointer to the pointer to the first node
+* @idx: the position to add the new node
+* @n: the data to be added to the new node
+* Return: address of the new node or NULL
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int index = 0;
+	listint_t *newptr, *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+	/*newnode->n = n; */
+	/*newnode->next = NULL; */
+	newptr = *head;
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	if (idx == 0)
+	{
+		newnode->next = newptr;
+		*head = newnode;
+		return (*head);
+	}
+		newnode->next = newptr;
+	if (index < idx)
+	{
+		for (index = 1; index < idx; index++)
+		{
+			newptr = newptr->next;
+		}
+	}
+	else if (index == idx)
+	{
+		newnode->next = newptr->next;
+		newptr->next = newnode;
+	}
+	else
+	{
+		free(newnode);
+		return (NULL);
+	}
+	newnode->next = newptr->next;
+	newptr->next = newnode;
+	return (newnode);
+}
