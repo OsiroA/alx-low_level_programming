e386b8a14219c3d1bf0c6cd2b739cc0d5bc4c179
OsiroA <osiroasunde@gmail.com>
2023-04-10 23:11:24 +0100

    added a placeholder content to the actual file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,1 +1,13 @@
-a
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @naac: not a conditioner
+ * @naav: not a vector
+ * Return: 0 or 1
+ */
+int main(int naac, char *naav[])
+{
+	return (1);
+	return (0);
+}
