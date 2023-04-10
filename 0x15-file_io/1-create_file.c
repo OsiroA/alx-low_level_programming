866e7dfae1dacc9bae50076a45c0d395d458e6b9
OsiroA <osiroasunde@gmail.com>
2023-04-10 19:21:14 +0100

    I changed the return value for when textcontent is NULL from -1 to 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 	{
 		close(filepointer);
-		return (-1);
+		return (1);
 	}
 
 	for (len = 0; text_content[len] != '\0'; len++)
