5b439556c76bfdc9f6fc415773c92c1ec4330b15
OsiroA <osiroasunde@gmail.com>
2023-05-18 15:34:34 +0100

    I added more checks for possible errors

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,36 +13,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp, *newNode;
 	unsigned int count = 0;
 
-	temp = *h;
+	if (h == NULL)
+		return (NULL);
 
+	temp = *h;
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
 	while (temp != NULL && count < idx - 1)
 	{
 		temp = temp->next;
 		count++;
 	}
-	if (count == idx - 1 || (count == 0 && idx == 0))
+	if (count == idx - 1)
 	{
+		if (temp == NULL)
+			return (NULL);
+
 		newNode = malloc(sizeof(dlistint_t));
 		if (newNode == NULL)
 			return (NULL);
 
 		newNode->n = n;
-		if (count == 0)
-		{
-			newNode->prev = NULL;
-			newNode->next = *h;
-			if (*h != NULL)
-				(*h)->prev = newNode;
-			*h = newNode;
-		}
-		else
-		{
-			newNode->prev = temp;
-			newNode->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = newNode;
-			temp->next = newNode;
-		}
+		newNode->prev = temp;
+		newNode->next = temp->next;
+
+		if (temp->next != NULL)
+			temp->next->prev = newNode;
+
+		temp->next = newNode;
 
 		return (newNode);
 	}
