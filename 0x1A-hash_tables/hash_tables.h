c4e43e9d45a7b2a55911c047ea6d884f9af1cf61
OsiroA <124213013+OsiroA@users.noreply.github.com>
2023-06-09 22:57:59 +0100

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -70,7 +70,7 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
-void hash_table_delete(hash_table_t *ht)
+void hash_table_delete(hash_table_t *ht);
 void hash_table_print(const hash_table_t *ht);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
